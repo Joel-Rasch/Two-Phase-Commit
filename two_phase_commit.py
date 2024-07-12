2e00c27c8b74a83179bcf3a7e642575519546e9a
Joel Rasch <108012760+Joel-Rasch@users.noreply.github.com>
2024-07-12 17:29:40 +0200

    Update two_phase_commit.py

--- two_phase_commit.py
+++ two_phase_commit.py
@@ -1,20 +1,22 @@
 from kazoo.client import KazooClient
 import psycopg
 
-
+# Initialize two phase commit class for transaction handling
 class TwoPhaseCommit:
+    # Initialize Zookeeper client and connect to server
+    # Connect to postgres DB
     def __init__(self, zk_hosts, db_configs):
         self.zk = KazooClient(hosts=zk_hosts)
         self.zk.start()
         self.db_connections = [psycopg.connect(**config, autocommit=False) for config in db_configs]
-
+# Begin of TPC creating a znode for each participant
     def begin_phase(self, transaction_path):
         participants = self.zk.get_children(f'{transaction_path}/participants')
         results = []
         for i, participant in enumerate(participants):
             results.append(self.begin_participant(transaction_path, participant, i))
         return all(results)
-
+# Set status for a given participant
     def begin_participant(self, transaction_path, participant, index):
         conn = self.db_connections[index]
         try:
@@ -24,7 +26,7 @@ class TwoPhaseCommit:
             return True
         except Exception as e:
             return False
-
+# creates a transcation node for each transaction and each node in ZooKeeper
     def create_transaction(self, root_name, transaction_name, queries):
         try:
             transaction_id = len(self.zk.get_children(f'{root_name}'))
@@ -41,7 +43,7 @@ class TwoPhaseCommit:
             return transaction_path
         except Exception as e:
             raise
-
+# If all 3 phases passed transaction either commits or aborts on all 3 DBs
     def execute_transaction(self, transaction_path):
         try:
             if (self.begin_phase(transaction_path) and
@@ -54,7 +56,7 @@ class TwoPhaseCommit:
         except Exception as e:
             self.abort_transaction(transaction_path)
             return f"Transaction aborted due to error: {e}"
-
+# Get all participants and queries to prepare
     def prepare_phase(self, transaction_path):
         queries = self.get_queries(transaction_path)
         participants = self.zk.get_children(f'{transaction_path}/participants')
@@ -64,7 +66,7 @@ class TwoPhaseCommit:
             results.append(self.prepare_participant(transaction_path, participant, i, queries))
 
         return all(results)
-
+# PRepare a given participant based on index and db connection set zk node to ready
     def prepare_participant(self, transaction_path, participant, index, queries):
         conn = self.db_connections[index]
         try:
@@ -76,7 +78,7 @@ class TwoPhaseCommit:
         except Exception as e:
             conn.rollback()
             return False
-
+# Commit transcation if all participants are ready
     def commit_phase(self, transaction_path):
         participants = self.zk.get_children(f'{transaction_path}/participants')
 
@@ -91,7 +93,7 @@ class TwoPhaseCommit:
         if success:
             self.zk.set(transaction_path, b'COMMITTED')
         return success
-
+# If not all participants are committed roll all databases back
     def commit_participant(self, transaction_path, participant, index):
         conn = self.db_connections[index]
         try:
@@ -101,7 +103,7 @@ class TwoPhaseCommit:
         except Exception as e:
             conn.rollback()
             return False
-
+# If commit failed set Abort status for each participant
     def abort_transaction(self, transaction_path):
         participants = self.zk.get_children(f'{transaction_path}/participants')
         for i, participant in enumerate(participants):
@@ -112,11 +114,11 @@ class TwoPhaseCommit:
             except Exception as e:
                 pass
         self.zk.set(transaction_path, b'ABORTED')
-
+# return the query from znode
     def get_queries(self, transaction_path):
         queries_path = f'{transaction_path}/queries'
         return [self.zk.get(f'{queries_path}/{node}')[0].decode() for node in self.zk.get_children(queries_path)]
-
+# After tpc ended clean all connections
     def cleanup(self):
         for conn in self.db_connections:
             conn.close()
@@ -130,11 +132,12 @@ if __name__ == "__main__":
         {'dbname': 'db', 'user': 'user', 'password': 'password', 'host': '127.0.0.1', 'port': '5434'}
     ]
     queries = [
-        '''INSERT INTO accounts (name, id_number, iban, balance) VALUES ('joughhfhhhhn', 'safgihjs', 0249662, 340.0)'''
+        '''INSERT INTO accounts (name, id_number, iban, balance) VALUES ('Max Mustermann', '1234', 5678, 340.0)'''
     ]
 
     tpc = TwoPhaseCommit(zk_hosts, db_configs)
     try:
+        #Define Transcation node as T_x and main Root as 'APP'
         transaction_path = tpc.create_transaction('APP', 'T', queries)
         result = tpc.execute_transaction(transaction_path)
         print(result)
